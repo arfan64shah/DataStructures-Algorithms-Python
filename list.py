7e81d93d09050728b7cc82d3903af5898be41a31
Arfan Shah <arfan.shah_2023@ucentralasia.org>
2023-01-18 23:18:48 +0600

    Added changes to list

--- list.py
+++ list.py
@@ -1,2 +1,20 @@
+#create a list
 list1 = [1, 2, 3, [1, 2, 3], 4, "I am arfan", 6.5]
+
+
+#operations on list
+list1.append(5)
+
+#print(list1)
+
+del(list1[7])
+
+#print(list1)
+
+#insert an element into a specific location
+list1.insert(2, 10)
+
 print(list1)
+# print(list1[3][0])
+# print(list1[5])
+
